44d37515b29a80c78ab62eafd08d4ec0173b8331
Jack Jansen <Jack.Jansen@cwi.nl>
2025-01-09 21:50:16 +0100

    Updates to build script.

--- scripts/buildpackage.sh
+++ scripts/buildpackage.sh
@@ -4,7 +4,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 preset=$1
-rm -rf build
+# Update repo
+git fetch --recurse-submodules
+git pull
+git submodule update --remote evanescent signals bin2dash sub
+rm -rf build installed
 cmake --preset $preset
 cmake --build --preset $preset
+cmake --install --preset $preset
 cpack --preset $preset
+realpath build/package/*
