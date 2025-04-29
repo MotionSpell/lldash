dab4d4dd9b8958961482eaa740fd5a6dacd514e4
Jack Jansen <Jack.Jansen@cwi.nl>
2025-04-29 14:45:30 +0200

    Need to set PATH before trying to run cwipc_pymodules_install.sh on Linux.

--- tests/setup_test_environment.sh
+++ tests/setup_test_environment.sh
@@ -5,14 +5,18 @@
 
 if [ $(uname) = "Linux" ]; then
     sudo apt install -y python3.12-venv
+    
     curl -L -o cwipc-built.tar.gz https://github.com/cwi-dis/cwipc/releases/download/nightly/cwipc-ubuntu2404-nightly-built.tar.gz
     (cd installed && tar xfv ../cwipc-built.tar.gz)
-    python3.12 -m venv .venv
-    source .venv/bin/activate
-    CWIPC_PYTHON=$(which python) cwipc_pymodules_install.sh || true
+    
     export PATH=$(pwd)/installed/bin:$PATH
     export LD_LIBRARY_PATH=$(pwd)/installed/lib:$LD_LIBRARY_PATH
     export SIGNALS_SMD_PATH=$(pwd)/installed/lib/
+    
+    python3.12 -m venv .venv
+    source .venv/bin/activate
+    CWIPC_PYTHON=$(which python) cwipc_pymodules_install.sh || true
+    
     if [ "${GITHUB_ACTIONS:-false}" == true ]; then
         echo "$(pwd)/installed/bin" >> $GITHUB_PATH
         echo "LD_LIBRARY_PATH=$(pwd)/installed/lib:$LD_LIBRARY_PATH" >> $GITHUB_ENV
