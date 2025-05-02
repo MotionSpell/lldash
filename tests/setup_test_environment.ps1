5a855d80096b2096ae0aacd950328b9465f11280
Jack Jansen <Jack.Jansen@cwi.nl>
2025-05-02 19:12:06 +0200

    Debug prints.

--- tests/setup_test_environment.ps1
+++ tests/setup_test_environment.ps1
@@ -14,7 +14,9 @@ $env:Path = $cwipc_bin_dir + ";" + $lldash_bin_dir + ";" + $env:Path
 python -m venv .venv
 & .\.venv\Scripts\Activate.ps1
 
-cwipc_pymodules_install.ps1
+"xxxjack path is now $env:Path"
+
+& $cwipc_bin_dir\cwipc_pymodules_install.ps1
 $Env:SIGNALS_SMD_PATH=$lldash_bin_dir
 
 if ($env:GITHUB_ACTIONS) {
