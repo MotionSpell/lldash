2715e85bf1f56e9c20d4fd20629a9c18b7d5e59c
Jack Jansen <Jack.Jansen@cwi.nl>
2025-05-03 14:22:04 +0200

    Check whether command script does work for installing Python modules.

--- tests/setup_test_environment.ps1
+++ tests/setup_test_environment.ps1
@@ -14,11 +14,12 @@ $env:Path = $cwipc_bin_dir + ";" + $lldash_bin_dir + ";" + $env:Path
 python -m venv .venv
 & .\.venv\Scripts\Activate.ps1
 
-& $cwipc_bin_dir\cwipc_pymodules_install.ps1
+cwipc_pymodules_install.bat
+
 $Env:SIGNALS_SMD_PATH=$lldash_bin_dir
 
 if ($env:GITHUB_ACTIONS) {
-    echo "SIGNALS_SMD_PATH=$Env:SIGNALS_SMD_PATH" | Out-File -FilePath $GITHUB_ENV -Encoding utf8 -Append
-    echo "$cwipc_bin_dir" | Out-File -FilePath $GITHUB_PATH -Encoding utf8 -Append
-    echo "$lldash_bin_dir" | Out-File -FilePath $GITHUB_PATH -Encoding utf8 -Append
+    echo "SIGNALS_SMD_PATH=$Env:SIGNALS_SMD_PATH" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+    echo "$cwipc_bin_dir" | Out-File -FilePath $Env:GITHUB_PATH -Encoding utf8 -Append
+    echo "$lldash_bin_dir" | Out-File -FilePath $Env:GITHUB_PATH -Encoding utf8 -Append
 }
