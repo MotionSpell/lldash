2a1884ef0b555ed7a883ceae14a41531873bb40d
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2025-05-08 17:03:01 +0200

    - Scan for mpd again (as oppsed to waiting for a certain time) - Scan stderr, and look for new syntax message - Determine whether server exit status is fine differently (Wnidows doesn't give -15 for SIGTERM)

--- tests/testlatency/testlatency_server.py
+++ tests/testlatency/testlatency_server.py
@@ -12,6 +12,7 @@ class ServerThread(threading.Thread):
         self.process : Optional[subprocess.Popen[str]] = None
         self.mpd_seen = threading.Semaphore(0)
         self.exit_status = -1
+        self.did_terminate = False
 
     def run(self):
         serverproc_stderr = None
@@ -27,21 +28,21 @@ class ServerThread(threading.Thread):
                 "--port", "9000"
             ],
             text=True,
-            stdout=subprocess.PIPE,
-            stderr=serverproc_stderr
+            stdout=serverproc_stdout,
+            stderr=subprocess.PIPE
         )
         reported_mpd_seen = False
         while True:
-            assert self.process.stdout
-            line = self.process.stdout.readline()
+            assert self.process.stderr
+            line = self.process.stderr.readline()
             if not line:
                 break
-            if serverproc_stdout:
-                serverproc_stdout.write(line)
+            if serverproc_stderr:
+                serverproc_stderr.write(line)
             line = line.strip()
             if self.args.verbose:
                 print(f"testlatency: server: Server output: {line}", file=sys.stderr)
-            if not reported_mpd_seen and "Added" in line and ".mpd" in line:
+            if not reported_mpd_seen and "method=PUT" in line and ".mpd" in line:
                 if self.args.verbose:
                     print(f"testlatency: server: MPD file seen in server output: {line}", file=sys.stderr)
                 self.mpd_seen.release()
@@ -49,19 +50,16 @@ class ServerThread(threading.Thread):
         self.exit_status = self.process.wait()
         if self.args.verbose:
             print("testlatency: server: Server finished with exit status:", self.exit_status, file=sys.stderr)
-        if self.exit_status == -15:
-            # Expected exit status for SIGTERM
+        if self.did_terminate:
+            # Expected exit status for SIGTERM, or 1 on Windows.
             self.exit_status = 0
         
     def stop(self):
         if self.process:
+            self.did_terminate = True
             self.process.terminate()
             
-    def wait_for_mpd(self, timeout : float = 0) -> bool:
-        # Alternative implementation: we wait for 2* seg_dur
-        wait_dur = 4 * self.args.seg_dur
-        time.sleep(wait_dur / 1000.0)
-        return True
+    def wait_for_mpd(self, timeout : Optional[float] = None) -> bool:
         ok = self.mpd_seen.acquire(timeout=timeout)
         if not ok:
             print("testlatency: server: MPD file not seen in server output, aborting...", file=sys.stderr)
